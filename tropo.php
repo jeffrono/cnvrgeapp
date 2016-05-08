12cc15471f3b28c2f9370d3201c8124846b60caa
Jeff Novich <jeffnovich@gmail.com>
2016-05-07 20:46:48 -0400

    tropo

---
+++ tropo.php
@@ -0,0 +1,14 @@
+<?php
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+
+# URL: cnvrge.herokuapp.com/tropo.php
+
+#get the transcript post from tropo
+
+
+# post it as a document note to HMH
+
+
+
+?>
