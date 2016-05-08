a425bdf1451060e684519eb54ebd314c8736faf5
Jeff Novich <jeffnovich@gmail.com>
2016-05-07 20:58:18 -0400

    logs

--- tropo.php
+++ tropo.php
@@ -1,8 +1,12 @@
 <?php
+error_log("hello, this is a test!");
+
 $json = file_get_contents('php://input');
+
+error_log($json);
+
 $obj = json_decode($json);
 
-error_log("hello, this is a test!");
 error_log($obj);
 
 # URL: cnvrge.herokuapp.com/tropo.php
