2b26748fb65d9e406abefc1c4883171f40ccc2f8
Jeff Novich <jeffnovich@gmail.com>
2016-05-07 21:23:56 -0400

    json

--- tropo.php
+++ tropo.php
@@ -1,21 +1,23 @@
 <?php
-error_log("hello, this is a test!");
 
 $json = file_get_contents('php://input');
-
-error_log($json);
-
 $obj = json_decode($json);
 
-error_log($obj);
+$transcript = $obj->{'result'}->{'transcription'};
 
-# URL: cnvrge.herokuapp.com/tropo.php
+
+error_log($transcript);
+
+# URL: cnvrgeapp.herokuapp.com/tropo.php
 
 #get the transcript post from tropo
 
 
 # post it as a document note to HMH
 
+
+#ngrok.io
+#postman
 
 
 ?>
